c9c320daa6239ac0d58a006492d29f15a315a2cc
Amreith <72003702+Amreithvenkatesh@users.noreply.github.com>
2021-02-27 20:26:43 +0530

    Update pdfRouter.js

--- Routes/pdfRouter.js
+++ Routes/pdfRouter.js
@@ -3,20 +3,16 @@ const router=express.Router();
 const {isAuth,Features} = require('../Middleware/isAuth');
 const puppeteer = require('puppeteer')
 const Link=require('../models/link')
-const uploadData={
-    uploadLinks:[]
-}
+let template=``
  
 async function printPDF() {
   const browser = await puppeteer.launch({ headless:false});
   const page = await browser.newPage();
-
   await page.goto('http://localhost:3000/pdf.html', {waitUntil: 'networkidle0'});
-  page.evaluvate(()=>{
-      const template=``
-
-      document.getElementById('links').innerHTML=data
-
+  await page.evaluate(()=>
+  {
+    let dom = document.querySelector('#links');
+    dom.innerHTML = "hiii"
   })
   const pdf = await page.pdf({ 
       format: 'A4',
@@ -34,25 +30,28 @@ router.get('/mock',Features,(req,res,next)=>{
     Link.find({saved:req.user.id})
         .select({Topic:1,Link:1,Department:1,LinkType:1,SubjectName:1})
          .then(result=>{
-             const template=``
+             console.log(result)
              result.forEach(element=>{
                  template+=`<div class="export-tile">
                                 <a href="${element.Link}">${element.Topic}</a>
-                                <div class="link-details"></div>    
+                                <div class="link-details">
+                                    <p> ${element.SubjectName}</p>
+                                    <p>${element.Department}</p>    
+                                </div>    
                             </div>`
              })
          })
          .catch(err=>{
-             console.log('error while pdf user get')
+             console.log(err)
          })
-    // printPDF()
-    //     .then(pdf => {
-    //        res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
-    //        res.send(pdf)
-    // })
-    // .catch(err=>{
-    //     res.send('error while generating');
-    // })
+    printPDF()
+        .then(pdf => {
+           res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
+           res.send(pdf)
+    })
+    .catch(err=>{
+        res.send('error while generating');
+    })
 })
 
 module.exports=router;
